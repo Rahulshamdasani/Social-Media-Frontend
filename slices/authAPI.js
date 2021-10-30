1a8eb88ad7bf3366e7244d20edfe505f04e5251b
Shamdasani, Rahul <rshamdas@iu.edu>
2021-10-30 17:35:57 -0400

    proxy in package json

--- slices/authAPI.js
+++ slices/authAPI.js
@@ -4,7 +4,7 @@ import { PURGE } from "redux-persist"
 export const authApi = createApi({
   reducerPath: 'authApi',
   baseQuery: fetchBaseQuery({ 
-    baseUrl: 'https://dashboard.heroku.com/auth/',
+    baseUrl: '/auth/',
     mode: 'no-cors',
     prepareHeaders: (headers, { getState }) => {
       const isAuthenticated = getState().auth.isAuthenticated
