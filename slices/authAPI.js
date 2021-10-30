fa2a58855673717fac6284d6696190fe68760b01
Shamdasani, Rahul <rshamdas@iu.edu>
2021-10-30 12:27:06 -0400

    Initial Commit
    Pulled main branch

--- slices/authAPI.js
+++ slices/authAPI.js
@@ -4,7 +4,7 @@ import { PURGE } from "redux-persist"
 export const authApi = createApi({
   reducerPath: 'authApi',
   baseQuery: fetchBaseQuery({ 
-    baseUrl: 'https://koobecaff.herokuapp.com/auth/',
+    baseUrl: '/auth/',
     prepareHeaders: (headers, { getState }) => {
       const isAuthenticated = getState().auth.isAuthenticated
       const token = getState().auth.token
--- slices/profileSlice.js
+++ slices/profileSlice.js
@@ -36,3 +36,5 @@ export const {
   setProfileInfo
 } = slice.actions
 export default slice.reducer
+
+
